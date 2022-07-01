6cf9e55e955e1132562adb1fa517504dea08f5bc
morvine <ivanry25@yandex.ru>
2022-07-01 20:40:46 +0300

    add: test

--- test/specs/browser.navigate.spec.js
+++ test/specs/browser.navigate.spec.js
@@ -2,16 +2,16 @@ const NavigatePage  = require('../pageobjects/navigate.page');
 
 
 describe('Navigate link panel', () => {
-    // it('should links have the correct url', async () => {
-    //     await NavigatePage.open();
+    it('should links have the correct url', async () => {
+        await NavigatePage.open();
+        
+        const link = await NavigatePage.getLink('News');
+        await expect(link).toHaveHref(`${process.env.BASE_URL}/news`);
+        // await expect(link).toHaveLinkContaining('news');
+        // var innerHTML = await LoginPage.loginAvatar.getHTML();
+        // console.log(innerHTML);
 
-    //     const link = await NavigatePage.getLink('News');
-    //     await expect(link).toHaveHref('https://loadtest.k8stage.worldchess.com/news');
-    //     // await expect(link).toHaveLinkContaining('news');
-    //     // var innerHTML = await LoginPage.loginAvatar.getHTML();
-    //     // console.log(innerHTML);
-
-    // });
+    });
 
     // it('should links have the correct name', async () => {
     //     await NavigatePage.open();
@@ -38,30 +38,25 @@ describe('Navigate link panel', () => {
 
     // });
 
-    it('should links have the correct name', async () => {
-        await NavigatePage.open();
+    // it('should links have the correct name', async () => {
+    //     await NavigatePage.open();
         
-        const navMenuName = ['Новости', 'Арена', 'Смотреть', 'Учиться', 'Сообщество', 'Магазин']
-        const navMenuHref = ['/news', '/', '/broadcasts', '/masterclass.worldchess.com', '/community', '/shop.worldchess.com']
-        
-
-        for(var i = 0; i < navMenuName.length-1; i++) {
-            var link = await NavigatePage.getLink(navMenuName[i])
-            link.click()
-            // let url = browser.getUrl()
-            // console.log('URLLL', url)
-            await expect(browser.getUrl()).toHaveTextContaining(navMenuHref[i])
-            browser.back()
-        }
+    //     const navMenuName = ['Новости', 'Арена', 'Смотреть', 'Учиться', 'Сообщество', 'Магазин']
+    //     const navMenuHref = ['/news', '/', '/broadcasts', '/masterclass.worldchess.com', '/community', '/shop.worldchess.com']
         
 
-        
-       
-
-    });
-
-
-
-    
-    
+    //     for(var i = 0; i < navMenuName.length-1; i++) {
+    //         let link = await NavigatePage.getLink(navMenuName[i])
+    //         link.click()
+    //         await browser.pause(5000)
+    //         var url = await browser.getUrl()
+    //         // console.log('URLLLLLLLLLL', url)
+    //         // url.then(function(result){
+    //         //     console.log('URLLLLLLLLLL', result)
+    //         // })
+    //         // await expect(browser).toHaveUrlContaining('loadtest')
+    //         await expect(url).toContain(navMenuHref[i])
+    //         browser.switchWindow('https://game-loadtest.k8stage.worldchess.com/')
+    //     }
+    // });
 });
