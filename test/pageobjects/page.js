8fb1aa15469fdc9910ac97d90c10376447878a96
morvine <ivanry25@yandex.ru>
2022-07-01 20:38:37 +0300

    add: enviroments

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -2,13 +2,14 @@
 * main page object containing all methods, selectors and functionality
 * that is shared across all page objects
 */
+require("dotenv").config({ path: __dirname +'/../enviroments/.env' });
+
 module.exports = class Page {
     /**
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
     open (path = '') {
-        return browser.url(`https://game-loadtest.k8stage.worldchess.com/${path}`)
-        // return browser.url(`https://the-internet.herokuapp.com/${path}`)
+        return browser.url(`${process.env.GAME_URL}/${path}`)
     }
 }
